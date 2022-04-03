256aceba8938f9e9894ebbdd82578198e96148c2
Jay <jayprakashdwivedi12@gmail.com>
2022-04-04 02:32:13 +0530

    update colors

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,9 +1,9 @@
 const contractAddress = "0xb7C94BB74B61f56a98d6e3C7D9b5128A7C775928"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
-const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
+const welcome_h1 = "Welcome to the Chubby BUddy NFT Project!!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = 'The CodeCats NFT Project is a decentralized, open-source project that aims to demonstrate how to develope and launch your own NFT Collection. Follow step by step on the <a href="https://youtube.com/codestackr"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.';
+const welcome_p = 'The Chubby BUddy NFT Project is a decentralized, collection of NFTs - unique digital collectibles, living on the Polygon Blockchain. 5,000 buddies have been programmatically generated from different of combinations, each with unique characteristics and different traits.';
 const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
 const h1_presale_mint = "Pre-Sale Minting Open!!";
 const h1_public_mint = "Public Minting Open!!";
